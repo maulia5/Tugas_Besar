3e4129220e7093dc9a00715d94caa134d00b1575
Maulia Hafifatun Solihah <155612320+maulia5@users.noreply.github.com>
2024-12-28 01:51:07 +0700

    Delete DalamProject directory

--- DalamProject/config_.example.py
+++
@@ -1,9 +0,0 @@
-class Config(object):
-    HOST = "localhost"
-    PORT = 3306
-    USER = "root"
-    PASSWORD = ""
-    DB = "flask"
-    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_RECORD_QUERIES = True
